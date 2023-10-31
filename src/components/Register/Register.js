ac5fdbd56e355c734a2949b5dd73ea80b97c41ec
Oleksandra Fogel <113315714+oleksafogel@users.noreply.github.com>
2023-10-31 13:37:00 +0100

    updated urls to connectn to backend

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends Component {
     }
 
     onSubmitRegister = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://wonder-star-app-backend.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Signin/SignIn.js
+++ src/components/Signin/SignIn.js
@@ -16,7 +16,7 @@ class SignIn extends Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://wonder-star-app-backend.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
